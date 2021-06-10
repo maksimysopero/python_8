7792f3ea1688a690178abb54ad20a6591a54589c
Max <78997401+maksimysopero@users.noreply.github.com>
2021-06-11 02:10:52 +0300

    Update python_8_1.py

--- python_8_1.py
+++ python_8_1.py
@@ -1,20 +1,22 @@
+#Логическое программирование/Математические выражения
+
 from kanren import run, var, fact
 from kanren.assoccomm import eq_assoccomm as eq
 from kanren.assoccomm import commutative, associative
 
-add = 'add'
+add = 'add' #определяем математические операции, которые мы собираемся использовать
 mul = 'mul'
 
 fact(commutative, mul)
-fact(commutative, add)
+fact(commutative, add) 
 fact(associative, mul)
 fact(associative, add)
 
-a, b = var('a'), var('b')
-Original_pattern = (mul, (add, 5, a), b)
+a, b = var('a'), var('b') # переменные
+Original_pattern = (mul, (add, 5, a), b) # сравниваем с оригиналом
 
 exp1 = (mul, 2, (add, 3, 1))
 exp2 = (add, 5, (mul, 8, 1))
 
 print(run(0, (a,b), eq(Original_pattern, exp1)))
-print(run(0, (a,b), eq(Original_pattern, exp2)))
+print(run(0, (a,b), eq(Original_pattern, exp2))) # вывод
